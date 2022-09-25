6b60039e9a798c044095ff2b4873451f2e8fbab1
Nikolay Chumakov <nklchumakov@gmail.com>
2022-09-25 23:53:17 +0300

    Init commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export const App = () => {
+  return (
+    <div className="App">
+      <h1>MacOS Web</h1>
+    </div>
+  )
+}
---
+++ src/modules/Desktop/Desktop.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+export const Desktop: FC = () => {
+    return (
+        <h1>MacOS Big Sur (web)</h1>
+    )
+}
